dd4f9e696f3164a919185d9dcc83aca66e1426b6
vitoria moraes <vicolmoraes@gmail.com>
2019-07-23 13:40:15 -0300

    tela inicial (landingPage)

---
+++ app/src/main/java/com/vicolmoraes/itauchallenge/LandingPageActivity.kt
@@ -0,0 +1,13 @@
+package com.vicolmoraes.itauchallenge
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class LandingPageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.landing_page_activity)
+
+    }
+}
