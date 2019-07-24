82a2135349f188c1f6b5f861eaaa37aff7ab5920
vitoria moraes <vicolmoraes@gmail.com>
2019-07-24 14:27:34 -0300

    alteracao json

--- app/src/main/java/com/vicolmoraes/itauchallenge/model/Usuario.kt
+++ app/src/main/java/com/vicolmoraes/itauchallenge/model/Usuario.kt
@@ -1,10 +1,18 @@
 package com.vicolmoraes.itauchallenge.model
 
+import com.google.gson.annotations.SerializedName
 import java.io.Serializable
 
 data class Usuario(
+    @SerializedName("nome")
     var nome: String,
+
+    @SerializedName("conta_corrente")
     var conta_corrente: Double,
+
+    @SerializedName("conta_poupanca")
     var conta_poupanca: Double,
+
+    @SerializedName("contatos")
     var contatos: ArrayList<String>
 ) : Serializable
